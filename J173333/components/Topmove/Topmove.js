f7c89c54f08ecb9313a0217484ef71653ef829d8
fuyhon <48897173+fuyhon@users.noreply.github.com>
2019-05-07 11:28:01 +0800

    Add files via upload

---
+++ J173333/components/Topmove/Topmove.js
@@ -0,0 +1,70 @@
+// components/Topmove/Topmove.js
+Component({
+  /**
+   * 组件的属性列表
+   */
+  properties: {
+
+  },
+
+  /**
+   * 组件的初始数据
+   */
+  data: {
+    TopMove:[{
+
+      name:"好好学习"
+    },
+      {
+        name: "天天向上"
+      },
+      {
+        name: "666"
+      },
+      {
+        name: "微信"
+      },
+      {
+        name: "风景"
+      },
+      {
+        name: "重庆"
+      },
+      {
+        name: "杭州"
+      },
+      {
+
+        name: "好好学习"
+      },
+      {
+        name: "天天向上"
+      },
+      {
+        name: "666"
+      },
+      {
+        name: "微信"
+      },
+      {
+        name: "风景"
+      },
+      {
+        name: "重庆"
+      },
+      {
+        name: "杭州"
+      },
+      {
+        name: "沙雕"
+      },
+    ]
+  },
+
+  /**
+   * 组件的方法列表
+   */
+  methods: {
+
+  }
+})
---
+++ J173333/components/swp/swp.js
@@ -0,0 +1,22 @@
+// components/swp/swp.js
+Component({
+  /**
+   * 组件的属性列表
+   */
+  properties: {
+
+  },
+
+  /**
+   * 组件的初始数据
+   */
+  data: { 
+    imgList: [{
+      imagePath: "/images/1-1.jpg",
+    }, {
+      imagePath: "/images/1-2.jpg",
+    }, {
+      imagePath: "/images/1-3.jpg",}
+    ]
+  }
+})
